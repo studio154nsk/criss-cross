b96a9e7d144d303ff66557544745fffa21b70214
studio154nsk <51887080+studio154nsk@users.noreply.github.com>
2024-11-21 00:42:41 +0300

    Create library.h

---
+++ library.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <iomanip>
+#include <set>
+#include <queue>
+#include <unordered_set>
+#include <random>
+#include <chrono>
+#include <utility>
+#include <stack>
+
+using namespace std;
